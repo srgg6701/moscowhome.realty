2c47bdcb367c4664bbff1389606a782cac1fba8f
srgg6701 <srgg67@gmail.com>
2016-03-18 20:30:08 +0300

    Настройка верхней половины.

--- original/assets/js/xtra.js
+++ original/assets/js/xtra.js
@@ -7,9 +7,13 @@ window.onload=function(){
     var i= 0,
         intrvl=setTimeout(function(){
             if(jQuery){
+
                 applyResizeChanges(jQuery);
+
                 var $=jQuery;
+
                 window.onresize=applyResizeChanges;
+
                 $('#test-box').on('dblclick', function(){
                     $(this).toggle('transparent')
                 });
@@ -26,19 +30,21 @@ window.onload=function(){
 
 function applyResizeChanges(jQuery){
     var found=jQuery('#found'),
-        topSlider=jQuery('#top-slider');
-    (function($){
-        console.log('$', $);
-        window.applyResizeChanges = function(){
-            var iw=window.innerWidth, place;
+        topSlider=jQuery('#top-slider'),
+        searchBox=jQuery('#search-box'),
+        $=jQuery;
+    window.applyResizeChanges = function(){
+        var windowWidth=window.innerWidth, place;
 
-            $('#test-box').html(iw);
-            console.log({ iw: iw, place: place });
+        $('#test-box').html(windowWidth); //console.log({ windowWidth: windowWidth, place: place });
 
-            place=(iw>=967)? 'after':'before';
-            $('#search-box')[place](found);
-
-            $('#contacts').before()
+        if(windowWidth>=967) {
+            searchBox.after(found);
+        }else{
+            searchBox.before(found);
+            $('#contacts').before(topSlider);
         }
-    }(jQuery));
+    };
+    applyResizeChanges();
 }
+
