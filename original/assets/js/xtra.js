30173ec8ba4de033e232161305f4b2f25060b8b4
srgg6701 <srgg67@gmail.com>
2016-03-21 20:49:41 +0300

    Настройка футера и заглушки на тексте.

--- original/assets/js/xtra.js
+++ original/assets/js/xtra.js
@@ -34,7 +34,8 @@ function applyResizeChanges(jQuery){
         searchBox=$('#search-box');
     window.applyResizeChanges = function(){
         var windowWidth=window.innerWidth, place,
-            sliderBlockDescrObj=$('.slider-block-descr-object').eq(0);
+            sliderBlockDescrObj=$('.slider-block-descr-object').eq(0),
+            objPhotoBlock=$('.object-photo-block').eq(0);
 
         console.info('applyResizeChanges resize, windowWidth', windowWidth);
 
@@ -44,9 +45,11 @@ function applyResizeChanges(jQuery){
             console.info('applyResizeChanges >=967, windowWidth: %c'+windowWidth, 'color:violet');
             searchBox.after(found);
             sliderBlockDescrObj.insertAfter($('.breadcrumbs-block').eq(0));
+            objPhotoBlock.insertAfter($('.object-tags-block').eq(0));
         }else{
             searchBox.before(found);
             sliderBlockDescrObj.insertAfter($('.owl-wrapper-outer').eq(0));
+            objPhotoBlock.insertBefore($('.form-object-block').eq(0));
         }
     };
     applyResizeChanges();
