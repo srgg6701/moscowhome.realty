718ac7a13dcdc12aab03f537f3afa118665d6c82
srgg6701 <srgg67@gmail.com>
2016-03-25 18:52:23 +0300

    Частичная настройка посёлка.

--- local/assets/js/xtra.js
+++ local/assets/js/xtra.js
@@ -82,46 +82,82 @@ function applyResizeChanges(jQuery){
         objectDescribeWrapper = $('#object-descr-textblock-wrapper'),
         sliderBlockDescrObj=$('#slider-block-descr-object'),
         searchBox=$('#search-box'),
-        handleBlock=function(target, place, actionCheck){//, actionDo
+        handleBlock=function(target, place, actionCheck, reverse){//, actionDo
             if(target.length&&place.length){
-                console.log('Both target & place have found',
-                    { target:target, place:place });
+                console.groupCollapsed('handleBlock');
+                    console.log('Both target & place have found',
+                        { target:target, place:place });
                 if(actionCheck=='has' && !place.has(target).length){
-                    console.log(target+' appendTo '+place);
+                    console.log(target);console.log('appendTo');console.log(place);
                     target.appendTo(place);
                 }else{
-                    var actionDo='insert';
-                    actionDo+=(actionCheck=='after')? 'Before':'After';
-                    if(target[actionCheck]()[0].id!=place[0].id) {
-                        console.log(place[actionCheck](target)[0].id+'=='+place[0].id,
-                            target + actionDo + place);
+                    var actionDo='insert',
+                        checkAction=(reverse)? 'before':'after',
+                        elementToGet=target[actionCheck]();
+                    actionDo+=(actionCheck==checkAction)? 'Before':'After';
+
+                    /*console.groupCollapsed('%ccheck target or place', 'background-color:yellow');
+                        console.log({
+                            '-1 actionCheck':actionCheck,
+                            '0 target':target,
+                            '1 place':place,
+                            '2 target[actionCheck]()[0]':target[actionCheck]()[0],
+                            '3 place[0]':place[0],
+                            arguments:arguments
+                        });
+                        console.trace();
+                    console.groupEnd();*/
+
+                    // target[after]()[0].id!=place[0].id
+                    //   target[insertBefore](place);
+                    // target[before]()[0].id!=place[0].id
+                    //   target[insertAfter](place);
+
+                    if(!elementToGet[0] ||
+                        elementToGet[0].id!=place[0].id ) {
+                        //console.log(place[actionCheck](target)[0].id+'=='+place[0].id);
+                        console.log('%cПерестановка', 'color: brown', {
+                            '0 target':target,
+                            '1 actionDo':actionDo,
+                            '2 place':place
+                        });
                         target[actionDo](place);
                     }
                 }
+                console.groupEnd();
             }
         };
     window.applyResizeChanges = function(){
         var windowWidth     =window.innerWidth,
-            objPhotoBlock   =$('.object-photo-block').eq(0),
             objTagsBlock    =$('.object-tags-block').eq(0),
             owlWrapperOuter =$('.owl-wrapper-outer').eq(0),
             formObjectBlock =$('.form-object-block').eq(0),
+            objPhotoBlock   =$('#object-photo-block'),
+            villageHeader   =$('#village-header'),
+            villageCard     =$('#village-card'),
+            villageInfo     =$('#village-info'),
             desktop = windowWidth>=mobilePoint;
 
         //console.info('applyResizeChanges resize, windowWidth', { windowWidth:windowWidth});
 
         $('#test-box').html(windowWidth); //console.log({ windowWidth: windowWidth, place: place });
-
+        // go desktop
         if(desktop) {
             //console.log('applyResizeChanges >='+mobilePoint+', windowWidth: %c'+windowWidth, 'color:violet', { searchBox:searchBox, action:'insertBefore', found: found, 'sliderBlockDescrObj':sliderBlockDescrObj, objPhotoBlock:objPhotoBlock, objectTagsBlock:objTagsBlock });
-            handleBlock(searchBox, found, 'after');
+            handleBlock(searchBox, found, 'after'); // defined on the top
             handleBlock(sliderBlockDescrObj, objectDescribeWrapper, 'has');
-            handleBlock(objPhotoBlock, objTagsBlock, 'before');
-        }else{
-            //console.log('applyResizeChanges >='+mobilePoint+', windowWidth: %c'+windowWidth, 'color:violet', { searchBox:searchBox, action:'insertAfter', found: found, 'sliderBlockDescrObj':sliderBlockDescrObj, objPhotoBlock:objPhotoBlock, objectTagsBlock:objTagsBlock });
+            // фото на стр. объекта
+            handleBlock(objPhotoBlock, objTagsBlock, 'after', true);
+            handleBlock(villageHeader, villageCard, 'after');
+            handleBlock(villageInfo, villageCard, 'has');
+        }else{ // go mobile
+            //console.log('applyResizeChanges <'+mobilePoint+', windowWidth: %c'+windowWidth, 'color:violet', { searchBox:searchBox, action:'insertAfter', found: found, 'sliderBlockDescrObj':sliderBlockDescrObj, objPhotoBlock:objPhotoBlock, objectTagsBlock:objTagsBlock });
             handleBlock(searchBox, found, 'before');
             handleBlock(sliderBlockDescrObj, owlWrapperOuter, 'before');
-            handleBlock(objPhotoBlock, formObjectBlock, 'after');
+            // фото на стр. объекта
+            handleBlock(objPhotoBlock, formObjectBlock, 'before', true);
+            handleBlock(villageHeader, villageCard, 'before');
+            handleBlock(villageInfo, villageHeader, 'has');
         }
         window.handleContactsPanel(desktop);
     };
