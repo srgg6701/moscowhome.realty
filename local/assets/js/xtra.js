aaf277d3bb6d202d790fbfd4a9492fb77a60bf39
srgg6701 <srgg67@gmail.com>
2016-03-25 12:12:15 +0300

    Добавлен обработчик позиции панели контактов.

--- local/assets/js/xtra.js
+++ local/assets/js/xtra.js
@@ -1,28 +1,43 @@
 window.onload=function(){
+
     if(!jQuery){
         var jq=document.createElement('script');
         document.getElementsByTagName('head')[0].appendChild(jq);
         jq.src="assets/js/libs/jquery-2.2.2.min.js";
-    }else{
-        var checkboxes = jQuery('#filters-set input[type="checkbox"]');
-        checkboxes.on('click', function(){
-            jQuery(this).toggleClass('checked');
-        })
     }
+
     var i= 0,
         intrvl=setTimeout(function(){
             if(jQuery){
-
                 applyResizeChanges(jQuery);
-
-                var $=jQuery;
+                var $=jQuery,
+                    checkboxes = $('#filters-set input[type="checkbox"]'),
+                    contactsPanel = $('#contacts'),
+                    classFixed = 'fixed',
+                    doc = $(document),
+                    contactsOffsetTop = contactsPanel.offset().top;
 
                 window.onresize=applyResizeChanges;
 
                 $('#test-box').on('dblclick', function(){
                     $(this).toggle('transparent')
                 });
+                checkboxes.on('click', function(){
+                    $(this).toggleClass('checked');
+                });
 
+                window.onscroll = function(){
+                    /*console.log({
+                        'document.scrollTop()':doc.scrollTop(),
+                        contactsOffsetTop:contactsOffsetTop });*/
+                    // Зафиксировать/расфиксировать панель контактов
+                    if(doc.scrollTop()>=contactsOffsetTop){
+                        if(!contactsPanel.hasClass(classFixed))
+                            contactsPanel.addClass(classFixed);
+                    }else if(contactsPanel.hasClass(classFixed)){
+                        contactsPanel.removeClass(classFixed);
+                    }
+                };
                 clearInterval(intrvl);
             }
             i++;
@@ -38,29 +53,47 @@ function applyResizeChanges(jQuery){
         found=$('#found'),
         objectDescribeWrapper = $('#object-descr-textblock-wrapper'),
         sliderBlockDescrObj=$('#slider-block-descr-object'),
-        searchBox=$('#search-box');
+        searchBox=$('#search-box'),
+        handleBlock=function(target, place, actionCheck){//, actionDo
+            if(target.length&&place.length){
+                console.log('Both target & place have found',
+                    { target:target, place:place });
+                if(actionCheck=='has' && !place.has(target).length){
+                    console.log(target+' appendTo '+place);
+                    target.appendTo(place);
+                }else{
+                    var actionDo='insert';
+                    actionDo+=(actionCheck=='after')? 'Before':'After';
+                    if(target[actionCheck]()[0].id!=place[0].id) {
+                        console.log(place[actionCheck](target)[0].id+'=='+place[0].id,
+                            target + actionDo + place);
+                        target[actionDo](place);
+                    }
+                }
+            }
+        };
     window.applyResizeChanges = function(){
-        var windowWidth=window.innerWidth, place,
-            objPhotoBlock=$('.object-photo-block').eq(0),
-            desktop = $(objectDescribeWrapper).find(sliderBlockDescrObj).length;
+        var windowWidth     =window.innerWidth, place,
+            objPhotoBlock   =$('.object-photo-block').eq(0),
+            objTagsBlock    =$('.object-tags-block').eq(0),
+            owlWrapperOuter =$('.owl-wrapper-outer').eq(0),
+            formObjectBlock =$('.form-object-block').eq(0);
 
-        //console.info('applyResizeChanges resize, windowWidth', windowWidth);
+        console.info('applyResizeChanges resize, windowWidth', { windowWidth:windowWidth});
 
         $('#test-box').html(windowWidth); //console.log({ windowWidth: windowWidth, place: place });
 
         if(windowWidth>=970) {
-            if(!desktop){
-                //console.info('applyResizeChanges >=970, windowWidth: %c'+windowWidth, 'color:violet', {'sliderBlockDescrObj':sliderBlockDescrObj, objectDescribeWrapper:objectDescribeWrapper});
-                searchBox.after(found);
-                sliderBlockDescrObj.appendTo(objectDescribeWrapper);
-                objPhotoBlock.insertAfter($('.object-tags-block').eq(0));
-            }
+            //if(!desktop){
+            console.log('applyResizeChanges >=970, windowWidth: %c'+windowWidth, 'color:violet', { searchBox:searchBox, action:'insertBefore', found: found, 'sliderBlockDescrObj':sliderBlockDescrObj, objPhotoBlock:objPhotoBlock, objectTagsBlock:objTagsBlock });
+            handleBlock(searchBox, found, 'after');
+            handleBlock(sliderBlockDescrObj, objectDescribeWrapper, 'has');
+            handleBlock(objPhotoBlock, objTagsBlock, 'before');
         }else{
-            if(desktop){
-                searchBox.before(found);
-                sliderBlockDescrObj.insertAfter($('.owl-wrapper-outer').eq(0));
-                objPhotoBlock.insertBefore($('.form-object-block').eq(0));
-            }
+            console.log('applyResizeChanges >=970, windowWidth: %c'+windowWidth, 'color:violet', { searchBox:searchBox, action:'insertAfter', found: found, 'sliderBlockDescrObj':sliderBlockDescrObj, objPhotoBlock:objPhotoBlock, objectTagsBlock:objTagsBlock });
+            handleBlock(searchBox, found, 'before');
+            handleBlock(sliderBlockDescrObj, owlWrapperOuter, 'before');
+            handleBlock(objPhotoBlock, formObjectBlock, 'after');
         }
     };
     applyResizeChanges();
